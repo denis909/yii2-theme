bda721bc59042ebecad233004f143e7d097c9852
denis909 <denis909@mail.ru>
2018-11-29 15:11:56 +0300

    n/a

--- grid/DataColumn.php
+++ grid/DataColumn.php
@@ -4,4 +4,17 @@ namespace denis909\yii\grid;
 
 abstract class DataColumn extends \yii\grid\DataColumn
 {
+
+	public $targetAttribute;
+
+    protected function renderDataCellContent($model, $key, $index)
+    {
+    	if ($this->targetAttribute)
+    	{
+    		return $model->{$this->targetAttribute};
+    	}
+
+    	return parent::renderDataCellContent($model, $key, $index);
+    }
+
 }
