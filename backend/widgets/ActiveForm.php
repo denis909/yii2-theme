422770334e3ad72547419cb8b1c2d29e8e5d2b57
denis909 <denis909@mail.ru>
2018-11-30 15:40:39 +0300

    n/a

--- backend/widgets/ActiveForm.php
+++ backend/widgets/ActiveForm.php
@@ -4,4 +4,7 @@ namespace backend\widgets;
 
 class ActiveForm extends \theme\widgets\ActiveForm
 {
+
+	public $fieldClass = 'backend\widgets\ActiveField';
+
 }
--- frontend/widgets/ActiveForm.php
+++ frontend/widgets/ActiveForm.php
@@ -4,4 +4,7 @@ namespace frontend\widgets;
 
 class ActiveForm extends \theme\widgets\ActiveForm
 {
+
+	public $fieldClass = 'frontend\widgets\ActiveField';
+
 }
