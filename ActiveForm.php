6512639fdc8271a28684589e2b2bf031ba7d81af
denis909 <denis909@mail.ru>
2019-10-02 21:14:58 +0300

    n/a

--- ActiveForm.php
+++ ActiveForm.php
@@ -2,9 +2,16 @@
 
 namespace denis909\theme;
 
+use yii\helpers\Html;
+
 class ActiveForm extends \yii\widgets\ActiveForm
 {
 
     public $fieldClass = ActiveField::class;
+
+    public function submitButton($name, array $options = [])
+    {
+        return Html::submitButton($name, $options);
+    }
     
 }
