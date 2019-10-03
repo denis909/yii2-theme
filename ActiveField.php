f3cd3bea4f3aac17ed35e4ecc97ef6f56e4ba904
denis909 <denis909@mail.ru>
2019-10-03 17:33:11 +0300

    n/a

--- ActiveField.php
+++ ActiveField.php
@@ -4,4 +4,15 @@ namespace denis909\theme;
 
 class ActiveField extends \yii\widgets\ActiveField
 {
+
+    public function checkbox($options = [], $enclosedByLabel = false)
+    {
+        if (!$enclosedByLabel)
+        {
+            $this->template = str_replace('{input}', '<br>{input}', $this->template);
+        }
+
+        return parent::checkbox($options, $enclosedByLabel);
+    }
+
 }
