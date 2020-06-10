9cfbfc3fffb25acfce0d7000e860fde8c16b1413
denis909 <denis909@mail.ru>
2020-06-10 12:02:10 +0300

    n/a

---
+++ ActionColumn.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace denis909\theme;
+
+class ActionColumn extends \denis909\yii\ActionColumn
+{
+}
---
+++ Html.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace denis909\theme;
+
+class Html extends \denis909\yii\Html
+{
+}
