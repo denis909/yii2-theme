a5ff2bd2e5e97e44ab32bd4d19244eb2ab20463b
denis909 <denis909@mail.ru>
2020-06-13 11:17:10 +0300

    n/a

--- MainLayout.php
+++ MainLayout.php
@@ -5,7 +5,7 @@ namespace denis909\theme;
 class MainLayout extends \yii\base\Widget
 {
 
-    public $copyright;
+    public $copyright = 'Copyright © 2019 - <a href="#">My Company</a>.';
 
     public $optionsMenu = [];
 
@@ -14,6 +14,14 @@ class MainLayout extends \yii\base\Widget
     public $breadcrumbs = [];
 
     public $user;
+
+    public $username;
+
+    public $userMenu = [];
+
+    public $accountMenu = [];
+
+    public $accountDescription;
 
     public $infoMessages = [];
 
@@ -28,5 +36,36 @@ class MainLayout extends \yii\base\Widget
     public $enableCard = true;
 
     public $cardTitle;
+
+    public $layoutOptions = [];
+
+    public $messageClass = AlertMessage::class;
+
+    public $assetsClass = Assets::class;
+
+    public function getParams()
+    {
+        return [
+            'copyright' => str_replace('{year}', date('Y'), $this->copyright),
+            'optionsMenu' => $this->optionsMenu,
+            'content' => $this->content,
+            'breadcrumbs' => $this->breadcrumbs,
+            'user' => $this->user,
+            'username' => $this->username,
+            'userMenu' => $this->userMenu,
+            'accountMenu' => $this->accountMenu,
+            'accountDescription' => $this->accountDescription,
+            'infoMessages' => $this->infoMessages,
+            'successMessages' => $this->successMessages,
+            'errorMessages' => $this->errorMessages,
+            'actionMenu' => $this->actionMenu,
+            'mainMenu' => $this->mainMenu,
+            'enableCard' => $this->enableCard,
+            'cardTitle' => $this->cardTitle,
+            'layoutOptions' => $this->layoutOptions,
+            'messageClass' => $this->messageClass,
+            'assetsClass' => $this->assetsClass
+        ];
+    }
 
 }
