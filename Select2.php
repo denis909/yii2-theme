997d0b92075b6698573a5bfcea873242fd24cce0
denis909 <denis909@mail.ru>
2020-07-30 10:32:27 +0300

    n/a

--- Select2.php
+++ Select2.php
@@ -2,6 +2,18 @@
 
 namespace denis909\theme;
 
+use Yii;
+
 class Select2 extends \kartik\select2\Select2
 {
+
+    public $bsVersion = '4';
+
+    public function init()
+    {
+        Yii::$app->params['bsVersion'] = 4;
+
+        parent::init();
+    }
+    
 }
